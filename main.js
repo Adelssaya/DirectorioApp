8378ca3c325722425855e3be81e23b1184c3caae
Axel-Nova <Lorenzonova62@gmail.com>
2025-05-14 11:31:52 -0600

    Add files via upload

--- main.js
+++ main.js
@@ -1,1 +1,15 @@
-console.log("Hola")
+// En JS tenemos las funciones de tipo flecha
+// utilizaremos fetch que es un metodo para consumir una api
+
+//Paso 1: funcion flecha
+const consumirApi = () => {
+    fetch("https://jsonplaceholder.typicode.com/users")
+//convertirmos la respuesta a tipo JSON
+    .then((Response) => Response.json())
+//los datos los vamos a mandar a la consola
+    .then((data) => console.log(data))
+//Descubrir que hacer en caso que no me corresponda
+    .catch((error) => console.log(error));
+};
+
+consumirApi();
